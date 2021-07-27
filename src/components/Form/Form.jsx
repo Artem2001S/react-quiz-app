e4f8a6d41bc6a52eb7a23c83ba73b7a97796336f
Artem Kazaryan <kazarynarm@mail.ru>
2021-07-27 19:03:19 +0300

    add Errors to Form component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,10 +3,23 @@ import PropTypes from 'prop-types';
 import Input from 'components/UI/Input/Input';
 import classes from './Form.module.scss';
 import Button from 'components/UI/Button/Button';
+import Errors from 'components/UI/Errors/Errors';
+import Title from 'components/UI/Title/Title';
 
-const Form = ({ submitBtnText, inputs, onInputChange, onSubmit }) => {
+const Form = ({
+  submitBtnText,
+  inputs,
+  onInputChange,
+  errors,
+  title,
+  onSubmit,
+}) => {
   return (
     <form className={classes.Form} onSubmit={onSubmit}>
+      <Title medium centered>
+        {' '}
+        {title}{' '}
+      </Title>
       {inputs?.map(({ validationData, ...input }) => (
         <Input key={input.id} onChange={onInputChange} {...input} />
       ))}
@@ -14,12 +27,15 @@ const Form = ({ submitBtnText, inputs, onInputChange, onSubmit }) => {
       <Button type="button" onClick={onSubmit} className={classes.SubmitBtn}>
         {submitBtnText}
       </Button>
+      {errors.length > 0 && <Errors errors={errors} />}
     </form>
   );
 };
 
 Form.propTypes = {
   inputs: PropTypes.array,
+  errors: PropTypes.array,
+  title: PropTypes.string,
   submitBtnText: PropTypes.string,
   onInputChange: PropTypes.func,
   onSubmit: PropTypes.func,
