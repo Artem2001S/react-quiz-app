3ef5f30e8d45ea8c9f6d62b8aba2d05c242e9568
Artem Kazaryan <kazarynarm@mail.ru>
2021-07-27 17:35:50 +0300

    add Form component

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Input from 'components/UI/Input/Input';
+import classes from './Form.module.scss';
+import Button from 'components/UI/Button/Button';
+
+const Form = ({ submitBtnText, inputs, onInputChange, onSubmit }) => {
+  return (
+    <form className={classes.Form} onSubmit={onSubmit}>
+      {inputs?.map((input) => (
+        <Input key={input.key} onChange={onInputChange} {...input} />
+      ))}
+
+      <Button type="button" onClick={onSubmit} className={classes.SubmitBtn}>
+        {submitBtnText}
+      </Button>
+    </form>
+  );
+};
+
+Form.propTypes = {
+  inputs: PropTypes.array,
+  submitBtnText: PropTypes.string,
+  onInputChange: PropTypes.func,
+  onSubmit: PropTypes.func,
+};
+
+export default Form;
