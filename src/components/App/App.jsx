24096c74f5ce241efcc3bfc0fcc7ae05fe486489
Artem Kazaryan <kazarynarm@mail.ru>
2021-07-27 16:03:25 +0300

    add Button component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { getIsLoadingSelector } from 'redux/userInterface/selectors';
 import Loader from 'components/UI/Loader/Loader';
 import Login from 'pages/Login';
 import Home from 'pages/Home';
 import PrivateRoute from 'shared/PrivateRoute';
+import Button from 'components/UI/Button/Button';
 
 function App() {
   const isLoading = useSelector(getIsLoadingSelector);
 
   return (
     <Router>
-      <Link to="/">Protected</Link>
-      <Link to="/login">Login page</Link>
-
       <Switch>
         <PrivateRoute path="/" exact>
           <Home />
