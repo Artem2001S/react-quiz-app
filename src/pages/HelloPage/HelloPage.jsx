c1532dbe0fc6913ff7f0179d1d6264c1d8e48edc
Artem Kazaryan <kazarynarm@mail.ru>
2021-07-28 10:29:41 +0300

    change HelloPage

--- src/pages/HelloPage/HelloPage.jsx
+++ src/pages/HelloPage/HelloPage.jsx
@@ -1,26 +1,40 @@
 import Title from 'components/UI/Title/Title';
+import { useAuth } from 'hooks/useAuth';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import classes from './HelloPage.module.scss';
 
 const HelloPage = () => {
+  const { isAuthorized } = useAuth();
+
   return (
     <div className={classes.Container}>
       <Title className={classes.Left} large>
         Welcome to quiz app.
       </Title>
-      <Title small>
-        Have an account ?{' '}
-        <Link className={classes.Link} to="/login">
-          Login
-        </Link>
-      </Title>
-      <Title small>
-        or{' '}
-        <Link className={classes.Link} to="/signup">
-          Sign up
-        </Link>
-      </Title>
+      {isAuthorized ? (
+        <Title small>
+          You're authorized, go to{' '}
+          <Link className={classes.Link} to="/dashboard">
+            Dashboard
+          </Link>
+        </Title>
+      ) : (
+        <>
+          <Title small>
+            Have an account ?{' '}
+            <Link className={classes.Link} to="/login">
+              Login
+            </Link>
+          </Title>
+          <Title small>
+            or{' '}
+            <Link className={classes.Link} to="/signup">
+              Sign up
+            </Link>
+          </Title>
+        </>
+      )}
     </div>
   );
 };
